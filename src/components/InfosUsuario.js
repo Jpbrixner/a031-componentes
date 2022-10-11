44565338ba2803b59c85a700dc4f40a6e1dd71c8
Jpbrixner <jpbrixner1993@gmail.com>
2022-10-11 16:01:22 -0300

    Exercício Resolvido

---
+++ src/components/InfosUsuario.js
@@ -0,0 +1,11 @@
+export default function InfosUsuário(){
+    return(
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2012/11/21/17/02/lion-66898_960_720.jpg" alt="Leão"
+            height={150}
+            width={150}/>
+            <h4>Leão</h4>
+
+        </div>
+    )
+}
